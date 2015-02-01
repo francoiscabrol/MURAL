eba161d2cf308b017787c82966f411a97b1cb481
francoiscabrol <francois.cabrol@live.fr>
2015-02-01 12:54:06 -0500

    Modify MelVi dependency and move the build script from build.sbt to project/build.scala

---
+++ project/Build.scala
@@ -0,0 +1,58 @@
+import sbt.Keys._
+import sbt._
+
+object MuralBuild extends Build {
+
+  //////////////////////////////////////////////////////////////////////////////
+  // PROJECT INFO
+  //////////////////////////////////////////////////////////////////////////////
+
+  val ORGANIZATION = "francoiscabrol"
+  val PROJECT_NAME = "mural"
+  val PROJECT_VERSION = "0.2.0"
+  val SCALA_VERSION = "2.11.4"
+
+  //////////////////////////////////////////////////////////////////////////////
+  // PROJECTS
+  //////////////////////////////////////////////////////////////////////////////
+
+  lazy val root = Project(
+    id = PROJECT_NAME,
+    base = file("."),
+    settings = muralSettings
+  ).dependsOn(melvi)
+
+  lazy val melvi = uri("git://github.com/francoiscabrol/MelVi")
+
+
+  //////////////////////////////////////////////////////////////////////////////
+  // SETTINGS
+  //////////////////////////////////////////////////////////////////////////////
+
+  lazy val muralSettings = Project.defaultSettings ++ basicSettings
+
+  lazy val basicSettings = Seq(
+    version := PROJECT_VERSION,
+    organization := ORGANIZATION,
+    scalaVersion := SCALA_VERSION,
+
+    libraryDependencies <++= scalaVersion(v =>
+      Seq("org.scala-lang" % "scala-actors" % v)
+    ),
+
+    javacOptions ++= Seq("-source", "1.8", "-target", "1.8", "-Xlint"),
+
+    initialize := {
+      val _ = initialize.value
+      if (sys.props("java.specification.version") != "1.8")
+        sys.error("Java 8 is required for this project.")
+    },
+    
+    unmanagedJars in Compile += Attributed.blank(file(System.getenv("JAVA_HOME") + "/jre/lib/ext/jfxrt.jar")),
+
+    connectInput in run := true,
+
+    fork in run := true
+
+  )
+}
--- src/main/scala/com/cabrol/francois/mural/Test.scala
+++ src/main/scala/com/cabrol/francois/mural/Test.scala
@@ -1,105 +1,21 @@
 package main.scala.com.cabrol.francois.mural
 
-import com.cabrol.francois.libjamu.musictheory.entity.scaleNote.{ Scale, Chord }
-import com.cabrol.francois.libjamu.musictheory.entity.note.Note
+import java.awt.{Point, Toolkit}
+
+import com.cabrol.francois.libjamu.musictheory.entity.scaleNote.{Chord, Scale}
 import com.cabrol.francois.melvi.factory.MelodyVisualiserFactory
 import com.cabrol.francois.melvi.view.VisualiserView
-import java.awt.{ Point, Toolkit }
 import com.cabrol.francois.mural.generator.rulebased.Generator
 import com.cabrol.francois.mural.generator.rulebased.method.Methods
+import com.cabrol.francois.mural.generator.rulebased.parameters.{HarmonicDefinition, _}
 
 import scala.collection.mutable
-import com.cabrol.francois.mural.generator.rulebased.parameters._
-import com.cabrol.francois.mural.generator.rulebased.parameters.HarmonicDefinition
-
-import java.io.File
-import javax.sound.midi.ShortMessage
-import javax.sound.midi.ShortMessage._
-import javax.sound.midi.MidiEvent
-import javax.sound.midi.MidiSystem
-import javax.sound.midi.Sequence
-import javax.sound.midi.SysexMessage
-import javax.sound.midi.MetaMessage
 
 /**
  * Created with IntelliJ IDEA.
  * User: francois * Date: 2014-02-04
  */
 object Test {
-
-  def saveMidi(file: File, notes: List[Note], play: Boolean) = {
-
-    val seq = new Sequence(Sequence.PPQ, 4);
-    val track = seq.createTrack();
-
-    // General MIDI sysex -- turn on General MIDI sound set
-    val b = Array[Byte](0xF0.asInstanceOf[Byte], 0x7E, 0x7F, 0x09, 0x01, 0xF7.asInstanceOf[Byte]);
-    var sm = new SysexMessage();
-    sm.setMessage(b, 6);
-    var me = new MidiEvent(sm, 0);
-    track.add(me);
-
-    // set tempo (meta event)
-    var mt = new MetaMessage();
-    val bt = Array[Byte](0x03, 0x00, 0x00);
-    mt.setMessage(0x51, bt, 3);
-    me = new MidiEvent(mt, 0);
-    track.add(me);
-
-    // set track name (meta event)
-    mt = new MetaMessage();
-    val trackName = new String("mural track");
-    mt.setMessage(0x03, trackName.getBytes(), trackName.length());
-    me = new MidiEvent(mt, 0);
-    track.add(me);
-
-    def makeEvent(comd: Int, chan: Int, one: Int, two: Int, tick: Int): MidiEvent = {
-      val a = new ShortMessage();
-      a.setMessage(comd, chan, one, two);
-      return new MidiEvent(a, tick);
-    }
-
-    // set omni on
-    track.add(makeEvent(CONTROL_CHANGE, 0, 0x7D, 0x00, 0));
-
-    // set poly on
-    track.add(makeEvent(CONTROL_CHANGE, 0, 0x7F, 0x00, 0));
-
-    // set instrument to Piano
-    track.add(makeEvent(PROGRAM_CHANGE, 0x00, 0x00, 0x00, 0));
-
-    var maxtick = 0
-    for (n <- notes) {
-      println(n)
-      val start = (n.getRhythmicNote.getStart * 10).asInstanceOf[Int]
-      val end = start + (n.getRhythmicNote.getDuration * 10).asInstanceOf[Int]
-      track.add(makeEvent(NOTE_ON, 0, n.getKey.getMidiKey,
-        n.getRhythmicNote.getVelocity,
-        start))
-      track.add(makeEvent(NOTE_OFF, 0, n.getKey.getMidiKey,
-        n.getRhythmicNote.getVelocity,
-        end))
-      if (maxtick < end)
-        maxtick = end
-    }
-
-    // set end of track (meta event)
-    mt = new MetaMessage();
-    mt.setMessage(0x2F, Array[Byte](), 0);
-    me = new javax.sound.midi.MidiEvent(mt, maxtick);
-    track.add(me);
-
-    // write the MIDI sequence to a MIDI file
-    MidiSystem.write(seq, 1, file);
-
-    if (play) {
-      val sequencer = MidiSystem.getSequencer();
-      sequencer.open();
-      sequencer.setSequence(seq);
-      sequencer.setTempoInBPM(220);
-      sequencer.start();
-    }
-  }
 
   def main(args: Array[String]) = {
     print("Start the generator execution test")
@@ -130,11 +46,9 @@ object Test {
 
       println("generation execution time:" + (System.currentTimeMillis() - beforeTime))
 
-      saveMidi(new File(s"mural$i.mid"), notes, false)
-
       visuViews.+=(MelodyVisualiserFactory.create(notes))
     }
- 
+
     def getVisuPos(index: Int): Point = {
       val screenSize = Toolkit.getDefaultToolkit().getScreenSize();
       (index match {
